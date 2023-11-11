a62e734ffcb0bcac5fd92147087e8c81c2664122
Maria <globalkmaria@gmail.com>
2023-11-11 23:29:24 +0900

    Edit : Mock user page

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,23 +1,20 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
 import Avatar from "@/app/component/Avatar/Avatar";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default async function UserPage() {
-  const session = await getServerSession(authOptions);
+type Props = {
+  params: {
+    username: string;
+  };
+};
 
-  if (!session) {
-    redirect("/auth/signIn");
-  }
-
+export default async function UserPage({ params: { username } }: Props) {
   return (
     <div className="flex flex-col items-center m-10">
-      <div className="w-40 mb-4">
-        <Avatar withRing />
+      {/* <div className="w-40 mb-4">
+        <Avatar user={session.user} withRing />
       </div>
       <h2 className="font-semibold text-lg">{session?.user?.username}</h2>
-      <h2 className="font-semibold text-lg">{session?.user?.email}</h2>
+      <h2 className="font-semibold text-lg">{session?.user?.email}</h2> */}
+      <h2 className="font-semibold text-lg">{username}</h2>
     </div>
   );
 }
